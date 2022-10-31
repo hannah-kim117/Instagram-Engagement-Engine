4746744fc52c7fdc4f9a916d62237f88fb0e5362
hannah-kim117 <hannahhgk98@gmail.com>
2022-10-31 14:21:24 -0400

    initial commit

---
+++ instagram_bot.py
@@ -0,0 +1,21 @@
+from instapy import InstaPy
+from instapy import smart_run
+
+my_username = '11.7khg_sample'
+my_password = 'Lot05usetoabmf'
+
+session = InstaPy(username = my_username, 
+	password = my_password,
+	headless_browser = False)
+
+with smart_run(session):
+	session.set_relationship_bounds(enabled=True,
+		delimit_by_numbers=True,
+		max_followers=500,
+		min_followers=30,
+		min_following=50)
+
+	session.set_do_follow(True, percentage=100)
+	session.set_dont_like(['nsfw', 'kia', 'ford'])
+
+	session.like_by_tags(['bmw', 'mercedes'], amount=10)
